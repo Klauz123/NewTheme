5cbc2e1909f6ccf2aa9a0670ae8d09b1332555b3
klaus.bruns <klaus.bruns@uni-oldenburg.de>
2018-05-24 15:11:16 +0200

    Testing Footer stuff
    Footer added

--- src/Providers/ThemeServiceProvider.php
+++ src/Providers/ThemeServiceProvider.php
@@ -3,15 +3,31 @@
 namespace Theme\Providers;
 
 use Plenty\Plugin\ServiceProvider;
+use IO\Extensions\Functions\Partial;
+use Plenty\Plugin\Events\Dispatcher;
+use Plenty\Plugin\ServiceProvider;
+use Plenty\Plugin\Templates\Twig;
 
 class ThemeServiceProvider extends ServiceProvider
 {
 
-    /**
-     * Register the service provider.
-     */
-    public function register()
-    {
+	/**
+	 * Register the service provider.
+	 */
+	public function register()
+	{
 
+	}
+
+    /**
+	 * Boot a template for the footer that will be displayed in the template plugin instead of the original footer.
+	 */
+	public function boot(Twig $twig, Dispatcher $eventDispatcher)
+    {
+        $eventDispatcher->listen('IO.init.templates', function(Partial $partial)
+        {
+           $partial->set('footer', 'Theme::content.ThemeFooter');
+        }, 0);
+        return false;
     }
 }
