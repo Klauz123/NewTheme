56d3c596c65646458bbbbbd7346f5dfec300582d
klaus.bruns <klaus.bruns@uni-oldenburg.de>
2018-06-13 21:54:58 +0200

    test
    test

--- src/Providers/ScriptServiceProvider.php
+++ src/Providers/ScriptServiceProvider.php
@@ -8,7 +8,7 @@ use Plenty\Plugin\Events\Dispatcher;
 use Plenty\Plugin\ServiceProvider;
 use Plenty\Plugin\Templates\Twig;
 
-class ThemeServiceProvider extends ServiceProvider
+class ScriptServiceProvider extends ServiceProvider
 {
 
 	/**
